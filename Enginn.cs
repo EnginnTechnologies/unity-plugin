8177deb3cb9b9b08b69dab79779c67934afefd1f
Yann Hourdel <yann@hourdel.fr>
2020-10-05 18:11:10 +0200

    init settings

---
+++ Enginn.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class Enginn
+{
+
+  private static string apiKey = EditorPrefs.GetString("EnginnApiKey");
+
+  static void saveApiKey() {
+    EditorPrefs.SetString("EnginnApiKey", apiKey);
+  }
+
+  [PreferenceItem("Enginn")]
+  public static void EnginSettings()
+  {
+    apiKey = EditorGUILayout.TextField("API Key", apiKey);
+    saveApiKey();
+  }
+
+  [MenuItem("Enginn/Hello")]
+  public static void SayHello()
+  {
+    Debug.Log("Hello ! :)");
+  }
+
+}
